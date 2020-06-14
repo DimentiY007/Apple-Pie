a4c406ba2ee1ea4224820ddf72448dfaff94c81b
Dmitry Stekvashov <dimastekv@gmail.com>
2020-06-14 22:16:58 +0300

    Initial Commit

---
+++ Apple Pie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Apple Pie
+//
+//  Created by Dmitriy Stekvashov on 14.06.2020.
+//  Copyright © 2020 Dmitriy Stekvashov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
