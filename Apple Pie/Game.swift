05011df1e1d0fb76be8ad60b5813a5e88b78d2fa
Dmitry Stekvashov <dimastekv@gmail.com>
2020-06-15 14:00:06 +0300

    created game extract

---
+++ Apple Pie/Game.swift
@@ -0,0 +1,16 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by Dmitriy Stekvashov on 15.06.2020.
+//  Copyright © 2020 Dmitriy Stekvashov. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    var word: String //guess of word
+    var incorrectMovesRemaining: Int //quntity of try
+    
+    
+}
--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -8,13 +8,73 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
+    //list words for guess
+    var listOfWords = [
+        "арбуз",
+        "аанаг",
+        "вертолет",
+        "гномик",
+        "домик"
+    ]
+    
+    //incoreect try count
+    let incorrectMovesAllowed = 7
+    
+    //count wins and lose
+    let totalWins = 0
+    let totalLosses = 0
+    
+    //instanse current game
+    var currentGame: Game!
+    
+    
+    
+    
+    
+    @IBOutlet weak var treeImageView: UIImageView!
+    
+    
+    @IBOutlet var letterButtons: [UIButton]!
+    
+    
+    @IBOutlet weak var coreectWordLabel: UILabel!
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+        //stat new game
+        newRound()
+    }
+    
+    //stat new game
+    func newRound(){
+        let newWord = listOfWords.removeFirst()
+        
+        currentGame = Game(word: newWord,
+                           incorrectMovesRemaining: incorrectMovesAllowed)
+    }
+    
+    //update interface
+    func updateUI(){
+        //update picture
+        let imageName = "Tree \(currentGame.incorrectMovesRemaining)"
+        let image = UIImage(named: imageName)
+        treeImageView.image = image
+        
+        //update scores
+        scoreLabel.text = "Выигрышей: \(totalWins), Проигрышей: \(totalLosses)"
+    }
+     
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
+    
 
 }
 
